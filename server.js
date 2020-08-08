e0c3c4e505a021b4854471d027b0215978594027
Yashika Bihani <yashikabihani7@gmail.com>
2020-08-08 08:15:55 +0530

    Creating a Room Id + Viewing our own video

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const { v4: uuidv4} = require('uuid');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    res.status(200).send('Hello World');
+    // res.status(200).send('Hello World');
+    // res.render('room');
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req,res) => {
+    res.render('room', { roomId: req.params.room })
 })
 
 server.listen(3030);
